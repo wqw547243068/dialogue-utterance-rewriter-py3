5ee1e1e513b8741e3594b8f23b303b6bcc727063
SivilTaram <qianlxc@outlook.com>
2020-03-02 11:10:41 +0800

    Fix print problem.

--- decode.py
+++ decode.py
@@ -267,12 +267,10 @@ class BeamSearchDecoder(object):
 
 def print_results(article, abstract, decoded_output):
     """Prints the article, the reference summmary and the decoded summary to screen"""
-    print
     ""
     tf.logging.info('ARTICLE:  %s', article)
     tf.logging.info('REFERENCE SUMMARY: %s', abstract)
     tf.logging.info('GENERATED SUMMARY: %s', decoded_output)
-    print
     ""
 
 
